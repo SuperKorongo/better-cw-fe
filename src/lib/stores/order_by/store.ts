372f9ac5f851274c739ce13f5c357810679aec19
a <a>
2025-06-02 15:44:54 +0200

    Removed unused stuff and changed default order by

--- src/lib/stores/order_by/store.ts
+++ src/lib/stores/order_by/store.ts
@@ -2,7 +2,6 @@ import { page } from '$app/state';
 import { DEFAULT_PAGINATION, type OrderBy } from '$lib/models/Pagination';
 import { writable } from 'svelte/store';
 
-const ORDER_BY_COOKIE_NAME = 'order_by';
 export const ORDER_BY_QUERY_PARAM = 'order_by';
 
 export const orderBy = (() => {
@@ -13,29 +12,16 @@ export const orderBy = (() => {
 
 		set: (orderBy: OrderBy) =>
 			update(() => {
-				setCookie(orderBy);
 				return orderBy;
 			}),
 
 		init: () =>
 			update(() => {
 				const orderBy = getOrderBy(page.url) ?? DEFAULT_PAGINATION.orderBy!;
-				setCookie(orderBy);
 				return orderBy;
 			})
 	};
 })();
-
-function setCookie(orderBy: OrderBy) {
-	const cookieExpirationDays = 30;
-	const expirationDate = new Date();
-	expirationDate.setDate(expirationDate.getDate() + cookieExpirationDays);
-
-	const value = `${ORDER_BY_COOKIE_NAME}=${orderBy.column}|${orderBy.direction}`;
-	const expiration = `expires=${expirationDate.toUTCString()}`;
-
-	document.cookie = `${value}; ${expiration}; Path=/; SameSite=Lax`;
-}
 
 export function getOrderBy(url: URL): OrderBy | null {
 	return getFromUrl(url) ?? null;
@@ -46,17 +32,5 @@ export function getFromUrl(url: URL): OrderBy | null {
 	if (!queryParamValue) return null;
 
 	const [column, direction] = queryParamValue.split('|');
-	return { column, direction } as OrderBy;
-}
-
-export function getFromCookie(): OrderBy | null {
-	if (typeof document === 'undefined') return null;
-
-	const orderByCookie = document.cookie.split('; ').find((cookie) => {
-		return cookie.split('=')[0] === ORDER_BY_COOKIE_NAME;
-	});
-	if (!orderByCookie) return null;
-
-	const [column, direction] = orderByCookie.split('=')[1].split('|');
 	return { column, direction } as OrderBy;
 }
