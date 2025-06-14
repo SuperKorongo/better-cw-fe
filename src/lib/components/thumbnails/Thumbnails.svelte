6e0345548f9746576802c1345d346973791ce777
a <a>
2025-06-14 13:28:30 +0200

    activate ad banner

--- src/lib/components/thumbnails/Thumbnails.svelte
+++ src/lib/components/thumbnails/Thumbnails.svelte
@@ -7,6 +7,7 @@
 	import { orderBy } from '$lib/stores/order_by/store';
 	import { search } from '$lib/stores/search/store';
 	import { getTranslation } from '$lib/translations';
+	import AdBanner from '../ad-banner/AdBanner.svelte';
 	import Thumbnail from '../thumbnail/Thumbnail.svelte';
 	import { events, type GetVideosFunc } from './events';
 	import FreeOnlyToggle from './FreeOnlyToggle.svelte';
@@ -82,6 +83,7 @@
 		<FreeOnlyToggle bind:value={freeVideosOnly} />
 	</div>
 	<div class="thumbnails-container">
+		<AdBanner />
 		{#each videos as video (video.uuid)}
 			<Thumbnail {video} />
 		{/each}
