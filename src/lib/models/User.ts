96738f5ec3e8b1441e616cc9a8f4471ed126fe8f
a <a@a.a>
2025-08-28 21:36:08 +0200

    updated dependencies

--- src/lib/models/User.ts
+++ src/lib/models/User.ts
@@ -11,5 +11,6 @@ export type User = {
 	readonly rating: number;
 	readonly totalRatings: number;
 	readonly hasOpenDisputes: boolean;
+	readonly hasPendingFriendRequests: boolean;
 	readonly sinceTimestamp: number;
 };
