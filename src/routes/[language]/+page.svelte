2f828fa599c6a04765aad91ec082797ca842a5ad
a <a@a.a>
2025-08-31 12:45:31 +0200

    gs

--- src/routes/[language]/+page.svelte
+++ src/routes/[language]/+page.svelte
@@ -23,4 +23,15 @@
 
 {#if !data.error}
 	<Videos videos={data.videos} {getVideosFunc} />
+{:else}
+	<div class="backend-error">Internal server error.<br />Please try again later.</div>
 {/if}
+
+<style>
+	.backend-error {
+		margin-top: 100px;
+		text-align: center;
+		font-size: 40px;
+		font-weight: bold;
+	}
+</style>
