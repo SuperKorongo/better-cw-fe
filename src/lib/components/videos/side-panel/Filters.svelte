b03b5c45f07172d7444ccfb2f15bc7eeac56af90
a <a@a.a>
2025-09-05 16:18:19 +0200

    loading feedback

--- src/lib/components/videos/side-panel/Filters.svelte
+++ src/lib/components/videos/side-panel/Filters.svelte
@@ -2,6 +2,7 @@
 	import { goto } from '$app/navigation';
 	import { Durations, Visibilities } from '$lib/models/Filters';
 	import { language } from '$lib/stores/language/store';
+	import { loading } from '$lib/stores/loading/store';
 	import { filters, MAX_MAX_PRICE } from '$lib/stores/video_filters/store';
 	import { getTranslation } from '$lib/translations';
 	import { getSearchAndOrderAndFiltersQueryParams } from '$lib/utils/utils';
@@ -21,10 +22,12 @@
 			duration
 		});
 
+		loading.set(true);
 		goto(`/${$language}` + getSearchAndOrderAndFiltersQueryParams());
 	};
 	const onResetFilters = () => {
 		filters.setEmpty();
+		loading.set(true);
 		goto(`/${$language}` + getSearchAndOrderAndFiltersQueryParams());
 	};
 </script>
